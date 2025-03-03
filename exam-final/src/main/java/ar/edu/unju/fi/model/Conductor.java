f37e0cb0f191a3807060529594ca63b9c982d766
belua <belua@MIRKOMATEO>
2025-03-03 17:22:36 -0300

    Validaciones solo en los model, y dep en pom

--- exam-final/src/main/java/ar/edu/unju/fi/model/Conductor.java
+++ exam-final/src/main/java/ar/edu/unju/fi/model/Conductor.java
@@ -4,18 +4,38 @@ import java.time.Period;
 
 import org.springframework.stereotype.Component;
 
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Component
 @Data
 public class Conductor {
-
+		@NotNull (message = "El Id del conductor no puede ser nulo")
 		private int id;
+		
+		@NotBlank
+		@Size(min=3, max=30, message="El nombre debe contener como mínimo 3 caracteres y como máximo 30 caracteres")
+		@Pattern(regexp= "[a-z A-Z]*", message="Debe ingresar únicamente letras")
 	    private String nombre;
+		
+		@NotBlank
+		@Size(min=3, max=30, message="El apellido debe contener como mínimo 3 caracteres y como máximo 30 caracteres")
+		@Pattern(regexp= "[a-z A-Z]*", message="Debe ingresar únicamente letras")
 	    private String apellido;
+		
+		@NotNull
+		@Past (message="La fecha de nacimiento debe ser una fecha pasada")
 	    private LocalDate fechaNac;
+	    
+	    @NotNull
 	    private String automovil;
+	    
+	    @NotBlank(message = "El tipo de automóvil no puede estar en blanco.")
+	    @Pattern(regexp = "^(Base|Luxe|Premium)$", message = "El tipo de automóvil debe ser 'Base', 'Luxe' o 'Premium'.")
 	    private String tipoAutomovil; // "X", "Luxe", "Premium"
 	    
 	    public int getEdad() {
--- exam-final/src/main/java/ar/edu/unju/fi/model/Viaje.java
+++ exam-final/src/main/java/ar/edu/unju/fi/model/Viaje.java
@@ -2,13 +2,18 @@ package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 @Component
 @Data
 public class Viaje {
-
+	@NotNull(message = "El ID del viaje no puede ser nulo.")
     private int id;
+    
+    @NotNull(message = "El tipo de viaje no puede ser nulo.")
     private String tipo; // "corta", "media", "larga"private TipoViaje tipo
+    
+    @NotNull
     private double costo; // Costo base del viaje
 
     //por ahora lo calculamos aqui luego lo podemos calcular en la base de datos
